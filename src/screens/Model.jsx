9ffa9e34edc945ce7e3af5d8c91bc20987f8996b
Lakshay Neem <124112721+lakshayneem@users.noreply.github.com>
2025-04-15 11:40:22 +0530

    Update Model.jsx

--- src/screens/Model.jsx
+++ src/screens/Model.jsx
@@ -30,7 +30,7 @@ export default function Model() {
     console.log("Data being sent to API:", formValues);  // 🔥 Debugging step
   
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://cshw-machine-learning.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formValues),
